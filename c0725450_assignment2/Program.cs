6b3527b8c9ed7bab8b42642c1f5075159858d8b2
Komalpreet Gill <komalgillc3@gmail.com>
2019-03-07 14:31:11 -0500

    commit lab3

--- c0725450_assignment2/Program.cs
+++ c0725450_assignment2/Program.cs
@@ -25,7 +25,7 @@ namespace c0725450_assignment2
     {
         public delegate void MyDelegate();
 
-        void Method1()
+        void Method1( int i)
         {
             Console.WriteLine("Method1"); 
             Console.ReadLine();
@@ -34,7 +34,7 @@ namespace c0725450_assignment2
         public void Method2()
         {
             MyDelegate myDelegate = new MyDelegate(Method1);
-            myDelegate(50);
+            myDelegate();
         }
     }
 }
