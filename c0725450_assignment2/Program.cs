08d6c24dea85f7caf65ddc04b042a4b30f9b5bab
Komalpreet Gill <komalgillc3@gmail.com>
2019-03-07 15:47:24 -0500

    commit lab7

--- c0725450_assignment2/Program.cs
+++ c0725450_assignment2/Program.cs
@@ -14,8 +14,8 @@ namespace c0725450_assignment2
             //CSD3354 Section1
             //Assignment2
             //March 7,2019
-            DelegateExercises a = new DelegateExercises();
-            a.Method3();
+            DelegateExercises delegateExercises = new DelegateExercises();
+            delegateExercises.Method3();
             Console.ReadLine();
 
 
@@ -28,17 +28,19 @@ namespace c0725450_assignment2
 
         int Method1(int intMethod1)
         {
-            return intMethod1 * 2;
+            return intMethod1 * 4;
         }
 
         int Method2(int intMethod1)
         {
-            return intMethod1 * 10;
+            return intMethod1 * 20;
         }
         public void Method4(MyDelegate myDelegate)
         {
-            int result = myDelegate(10);
-            Console.WriteLine(result);
+            for(int i = 1; i <= 5; i++)
+            {
+                System.Console.Write(myDelegate(i) + " ");
+            }
         }
 
         public void Method3()
