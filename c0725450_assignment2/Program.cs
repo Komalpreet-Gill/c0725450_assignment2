4626e5bf8d6fdfea24e6652b668c14a2f6160a20
Komalpreet Gill <komalgillc3@gmail.com>
2019-03-07 16:03:02 -0500

    commit lab9

--- c0725450_assignment2/Program.cs
+++ c0725450_assignment2/Program.cs
@@ -14,27 +14,15 @@ namespace c0725450_assignment2
             //CSD3354 Section1
             //Assignment2
             //March 7,2019
-            DelegateExercises delegateExercises = new DelegateExercises();
-            delegateExercises.Method3();
-            Console.ReadLine();
 
 
         }
     }
     public delegate void MyDelegate();
 
-    public class DelegateExercises
+    public class DelegateExercises : MyDelegate
     {
-        
-        void Method1()
-        {
-            System.Console.WriteLine("Method1");
-        }
 
-        public void Method3()
-        {
-            MyDelegate myDelegate = new MyDelegate(Method1);
-            myDelegate();
-        }
     }
+
 }
