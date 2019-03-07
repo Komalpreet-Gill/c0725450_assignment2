4b7310ba1da41bcb4c64b1716140542ddd1a6666
Komalpreet Gill <komalgillc3@gmail.com>
2019-03-07 13:55:38 -0500

    Commit lab1

--- c0725450_assignment2/Program.cs
+++ c0725450_assignment2/Program.cs
@@ -10,6 +10,31 @@ namespace c0725450_assignment2
     {
         static void Main(string[] args)
         {
+            //Student name-Komalpreet Gill(c0725450)
+            //CSD3354 Section1
+            //Assignment2
+            //March 7,2019
+            DelegateExercises a = new DelegateExercises();
+            a.Method2();
+
+
+        }
+    }
+
+    public class DelegateExercises
+    {
+        public delegate void MyDelegate();
+
+        void Method1()
+        {
+            Console.WriteLine("Method1"); 
+            Console.ReadLine();
+        }
+
+        public void Method2()
+        {
+            MyDelegate myDelegate = new MyDelegate(Method1);
+            myDelegate();
         }
     }
 }
